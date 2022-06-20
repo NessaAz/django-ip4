10de2836ea761b5f1fe595d52d8e01849ff34b07
NessaAz <vanessa.azenwa@gmail.com>
2022-06-20 17:22:18 +0300

    add post Comment model class

--- outside/models.py
+++ outside/models.py
@@ -91,4 +91,13 @@ class Post(models.Model):
     
     
     
-    
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
+    comment = models.TextField()
+    likes = models.ManyToManyField(User, blank=True, related_name='comment_likes')
+    posted_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.comment    
