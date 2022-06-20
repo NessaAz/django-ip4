d656757b59c9fb2bc2cb0589568b86ff239eb0e7
NessaAz <vanessa.azenwa@gmail.com>
2022-06-20 17:04:46 +0300

    add neoghbourhood model class

--- outside/models.py
+++ outside/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    occupants = models.IntegerField(default=1, blank=True, null=False)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @classmethod
+    def get_neighborhood(cls, id):
+        neighborhood = cls.objects.get(id=id)
+        return neighborhood
+
+    def create_neighborhood(self):
+        self.save()
+        return self
+
+    @classmethod
+    def delete_neighborhood(cls, id):
+        neighborhood = cls.objects.get(id=id).delete()
+        return neighborhood
+
+    @classmethod
+    def update_neighborhood(cls, obj):
+        neighborhood = cls.get_neighborhood(obj.id)
+        neighborhood.name = obj.name
+        neighborhood.location = obj.location
+        neighborhood.save()
+        return neighborhood
+
+    def update_occupants(self, occupants):
+        self.occupants += occupants
+        self.save()
+        return self
+
+    def __str__(self):
+        return self.location        
