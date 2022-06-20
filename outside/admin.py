4c8026cc6eecead55613bb7a530d7bb632d1d012
NessaAz <vanessa.azenwa@gmail.com>
2022-06-20 17:26:56 +0300

    register all 5 model classess in admin site

--- outside/admin.py
+++ outside/admin.py
@@ -2,3 +2,12 @@ from django.contrib import admin
 from .models import *
 
 
+admin.site.register(Neighborhood)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
+
+
+
+
